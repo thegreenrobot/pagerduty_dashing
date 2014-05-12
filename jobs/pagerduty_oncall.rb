20e914bb2a1171e370aef9f8e28b02bc3fe79a8d
David <davidshackelford@gmail.com>
2014-05-12 14:52:18 -0700

    Handle multiple escalation policies
    Now you can pass in an array of EPs in your secrets.json:
    
    ```
        "escalation_policies": {
            "policy1": "blksdhfs",
            "policy2:" "kjhsldfkjhsd"
        }
    ```

--- jobs/pagerduty_oncall.rb
+++ jobs/pagerduty_oncall.rb
@@ -1,5 +1,6 @@
 require 'faraday'
 require 'json'
+require 'pp'
 # make sure your secrets.json has an "escalation_policy":"blahblah" entry
 
 data_file = './lib/secrets.json'
@@ -7,32 +8,37 @@ parsed_data = JSON.parse( IO.read( data_file ))
 
 url = parsed_data['url']
 api_key = parsed_data['api_key']
-escalation_policy = parsed_data['escalation_policy']
 
-oncalls = {}
+# empty array of oncalls
+oncall = {}
+
+# for each ep....
+parsed_data['escalation_policies'].each do |key, value|
+  oncall[key] = value
+end
 
 SCHEDULER.every '5s' do
+  oncall.each do |key,value|
     conn = Faraday.new(:url => "#{url}") do |faraday|
       faraday.request :url_encoded
       faraday.adapter Faraday.default_adapter
       faraday.headers['Content-type'] = 'application/json'
       faraday.headers['Authorization'] = "Token token=#{api_key}"
-      faraday.params['since'] = Time.now.utc.iso8601()
-      faraday.params['until'] = (Time.now.utc + 60).iso8601()
    end
 
-  response = conn.get "/api/v1/escalation_policies/#{escalation_policy}/on_call"
+  response = conn.get "/api/v1/escalation_policies/#{value}/on_call"
+  pp(response)
   if response.status == 200
     oncall_result = JSON.parse(response.body)
     #Note: this is currently broken for group alerting. Need to make it just look for the first entry where level:1, then where level:2
-    primary = oncall_result['escalation_policy']['on_call'][0]['user']['name']
-    secondary = oncall_result['escalation_policy']['on_call'][1]['user']['name']
+    user_name = oncall_result['escalation_policy']['on_call'][0]['user']['name']
   else
     user_name = 'John Doe'
   end
 
-  send_event("primary-name", { text: primary})
-  send_event("backup-name", { text: secondary})
+  send_event("#{key}-name", { text: user_name})
+  end
+
+
 
 end
-
